0b1c90687443eeda736bc5951853c637c7880d10
alanurdgz <nalan3947@gmail.com>
2021-09-13 18:36:47 -0500

    update

--- app/src/main/java/com/example/fragmentosc/MainActivity.kt
+++ app/src/main/java/com/example/fragmentosc/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.fragmentosc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction().add(R.id.container, inicioFragment()).commit()
+        findViewById<Button>(R.id.btnFragmentoA).setOnClickListener{
+            replaceFragment(fragmentA())
+        }
+        findViewById<Button>(R.id.btnFragmentoB).setOnClickListener{
+            replaceFragment(fragmentB())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            setCustomAnimations(
+                R.anim.slide_right,
+                R.anim.slide_out_left,
+                R.anim.slide_left,
+                R.anim.slide_out_right)
+            replace(R.id.container, fragment)
+            commit()
+        }
     }
 }
